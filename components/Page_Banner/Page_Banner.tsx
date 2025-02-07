9067c104d54822c20994644f26b3c70f3f74d187
aixigo_ekilic <emrullah.kilic@aixigo.com>
2025-02-07 23:49:40 +0300

    updated navbar

--- components/Page_Banner/Page_Banner.tsx
+++ components/Page_Banner/Page_Banner.tsx
@@ -1,29 +1,34 @@
 import Image from 'next/image';
 import styles from './PageBanner.module.css';
 import logo from '../../assets/logo.webp';
-import whatsapp from '../../assets/banner/whatsapp.png';
-import gallery from '../../assets/banner/gallery.png';
-import who from '../../assets/banner/who.png';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faContactCard, faImage, faQuestion } from '@fortawesome/free-solid-svg-icons';
+import { useRef } from 'react';
 
 export default function PageBanner() {
+    const items = useRef<HTMLDivElement>(null);
     return (
         <div style={styles} className={styles.banner}>
             <div className={styles.logo}>
-                <Image src={logo} alt="LOL" width={210} height={53} />
+               <Link href={'/'}> <Image src={logo} alt="LOL" width={210} height={53} /></Link>
             </div>
             <div className={styles.banner_actions}>
-                <button>
-                    <Image src={whatsapp} alt="LOL" width={25} height={25} />
-                    Kontakt
-                </button>
-                <button>
-                    <Image src={gallery} alt="LOL" width={25} height={25} />
-                    Galerie
-                </button>
-                <button>
-                    <Image src={who} alt="LOL" width={25} height={25} />
-                    VATAN
-                </button>
+                <FontAwesomeIcon icon={faBars} size={'2x'} id={styles.menu} />
+                <div id={styles.items} ref={items}>
+                    <Link href={'/'}>
+                        <FontAwesomeIcon icon={faContactCard} size={'2x'} />
+                        <h5>Kontakt</h5>
+                    </Link>
+                    <Link href={'/'}>
+                        <FontAwesomeIcon icon={faImage} size={'2x'} />
+                        <h5>Galerie</h5>
+                    </Link>
+                    <Link href={'/'}>
+                        <FontAwesomeIcon icon={faQuestion} size={'2x'} beat />
+                        <h5>Vatan</h5>
+                    </Link>
+                </div>
             </div>
         </div>
     );
